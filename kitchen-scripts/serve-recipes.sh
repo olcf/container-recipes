5051f92ce2a9a6aca43d34ac4fa3ae221537c1eb
atj <adambsimpson@gmail.com>
2018-01-23 13:17:41 -0500

    Update master with remote before committing updated registry list

--- kitchen-scripts/serve-recipes.sh
+++ kitchen-scripts/serve-recipes.sh
@@ -43,6 +43,7 @@ git config --global user.name "atj"
 
 # Update registry list markdown
 echo -e ${REGISTRY_LIST} > ${TOP_DIR}/REGISTRY_LIST.md
+git checkout -B master origin/master
 git add ${TOP_DIR}/REGISTRY_LIST.md
 git commit -m "Updating registry list"
 git push https://atj:${GIT_TOKEN}@code.ornl.gov/olcf/container-recipes master
