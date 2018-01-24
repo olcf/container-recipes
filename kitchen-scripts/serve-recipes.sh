ce2966aaa8e0a4eebc1657cc991b06df68137b80
atj <atj@ornl.gov>
2018-01-24 18:36:09 -0500

    Don't fail CI if nothing to commit

--- kitchen-scripts/serve-recipes.sh
+++ kitchen-scripts/serve-recipes.sh
@@ -46,6 +46,5 @@ echo -e "${REGISTRY_LIST}" > ${TOP_DIR}/REGISTRY_LIST.md
 git checkout -B master origin/master
 git add ${TOP_DIR}/REGISTRY_LIST.md
 git commit -m "Updating registry list"
-if [ $? -eq 0 ]; then
+git diff-index --quiet HEAD || git commit -m 'bla' && \
     git push https://atj:${GIT_TOKEN}@code.ornl.gov/olcf/container-recipes master
-fi
