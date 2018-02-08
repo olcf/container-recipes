b458f130cec09fcfb3c42b27b8dc2b7d6e2dd778
atj <adambsimpson@gmail.com>
2018-02-08 12:39:25 -0500

    Add summit support

--- summit/centos-7/2018-02-08/summit-rpm.sh
+++ summit/centos-7/2018-02-08/summit-rpm.sh
@@ -8,5 +8,5 @@ cp /ccs/packages/IBM/PRPQ-4Q17-Update/smpi/ibm_smpi-pami_devel-10.02.00.00prpq-r
 cp /ccs/packages/IBM/PRPQ-4Q17-Update/smpi/ibm_smpi_lic_s-10.02.00dev0-rh7_20171107.ppc64le.rpm         ./summit_rpm
 cp /ccs/packages/IBM/PRPQ-4Q17-Update/smpi/ibm_smpi-devel-10.02.00.00prpq-rh7_20171117.ppc64le.rpm      ./summit_rpm
 
-tar -zcvf ./summit_rpm.tar.gz ./summitdev_rpm
+tar -zcvf ./summit_rpm.tar.gz ./summit_rpm
 rm -rf ./summit_rpm
