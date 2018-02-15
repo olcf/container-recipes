6a70fad81455b47bffb4ac0f4569bd4ef005570f
AdamSimpson <adambsimpson@gmail.com>
2018-02-14 21:48:45 -0500

    Update to use smpi with jsm bundled

--- summit/centos-7/2018-02-08/summit-rpm.sh
+++ summit/centos-7/2018-02-08/summit-rpm.sh
@@ -3,7 +3,8 @@
 mkdir ./summit_rpm
 
 # Spectrum MPI
-cp /ccs/packages/IBM/02-2018/SMPI/ibm_smpi-10.2.0.0-20180110-rh7.ppc64le.tar.gz ./summit_rpm
+mkdir ./summit_rpm/smpi
+cp /ccs/packages/IBM/02-2018/SMPI/ibm_smpi-10.2.0.0-20180110-rh7.ppc64le.tar.gz ./summit_rpm/smpi
 
 # Mellanox OFED - directory created by mounting MLNX_OFED_LINUX-4.1-4.1.5.2-rhel7.4alternate-ppc64le.iso
 cp -r /ccs/packages/IBM/PRPQ-4Q17/mellanox ./summit_rpm
@@ -13,6 +14,9 @@ rm ./summit_rpm/mellanox/RPMS/mft-4.7.1-7.ppc64le.rpm
 rm ./summit_rpm/mellanox/RPMS/mlnx-fw-updater-4.1-4.1.5.2.ppc64le.rpm
 rm ./summit_rpm/mellanox/RPMS/openmpi-2.1.2a1-1.41102.ppc64le.rpm
 rm ./summit_rpm/mellanox/RPMS/mlnx-ofa_kernel-devel-4.1-OFED.4.1.4.1.5.1.g773619a.rhel7u4alternate.ppc64le.rpm
+
+tar -zcvf ./summit_rpm/mellanox.tar.gz ./summit_rpm/mellanox
+rm -rf ./summit_rpm/mellanox
 
 tar -zcvf ./summit_rpm.tar.gz ./summit_rpm
 rm -rf ./summit_rpm
